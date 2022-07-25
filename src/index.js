420d531da16082f44bd6ef1cfc542925874e3f37
vietanh <ungvietanh120799@gmail.com>
2022-07-26 00:55:13 +0700

    redux persistor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 // import ReactDOM from "react-dom";
 import App from "./App";
-import store from "./redux/store";
 import { Provider } from "react-redux";
+import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 
 import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
 const root = createRoot(container);
+let persistor = persistStore(store);
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
   //   <Provider store={store}>
   //   <App />
@@ -33,3 +38,5 @@ root.render(
 //     <App />
 //   </Provider>
 // )
+
+// ...
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reactionListReducer from "../components/reactionSlice";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
+// const store = configureStore({
+//   reducer: {
+//     reactionList: reactionListReducer.reducer,
+//   },
+// });
+
+// export default store;
+const reducers = combineReducers({
+  reactionList: reactionListReducer.reducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: {
-    reactionList: reactionListReducer.reducer,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
 
 export default store;
